a2f2af84a4317fcc182d0ac1d800cc3a9becd580
Subashini <125429575+suba-shini7@users.noreply.github.com>
2026-07-21 21:28:54 +0530

    Delete download.py

--- download.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoTokenizer
-
-MODEL_NAME = "meta-llama/Meta-Llama-3-8B-Instruct"
-
-print("Downloading tokenizer...")
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-
-print("Tokenizer downloaded!")
